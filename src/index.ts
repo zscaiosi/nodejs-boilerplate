4a5c9a7a2805f9806815e9959bf91ff1c834b9a3
caio.saldanha <caio.saldanha@dotzmkt.com.br>
2020-07-02 13:00:57 -0300

    fix: exporting;

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-// Makes an import from node API after installed @types/node
-import * as path from 'path';
-// Componets imports
-import {
-    Default,
-    DefaultInterface
-} from './entities/index';
-// Here is the entry point for the module, where other modules will call and have access to its APIs
-// Replace with your module name, signature and implementation
-const main = (opts?: any): any => {
-    let d = new Default();
-
-    d.setDefault(1);
-
-    return d.getDefault();
-};
-
-export default main;
+export * from './entities/index';
